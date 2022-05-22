2d045e200330740cec8f1f8d80d6fbb349637558
Felipe Simões da Rocha <Felipe.simoesdarocha@gmail.com>
2022-05-22 15:19:19 -0300

    new component menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './components/user/user.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { ServicesComponent } from './core/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    MenuComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
